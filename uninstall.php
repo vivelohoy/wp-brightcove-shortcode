e005ad3cdca57641d80e117b564b7831d3fb91b5
Nick Bennett <nbennett@vivelohoy.com>
2015-01-20 12:30:17 -0600

    Fixed uninstall.php to reference the correct options name.

--- uninstall.php
+++ uninstall.php
@@ -4,8 +4,8 @@ if( !defined( 'WP_UNINSTALL_PLUGIN' ) ) {
     exit;
 }
 
-if( get_option( 'wp_brightcove_shortcode' ) != false ) {
-    delete_option( 'wp_brightcove_shortcode' );
+if( get_option( 'wp_brightcove_shortcode_options' ) != false ) {
+    delete_option( 'wp_brightcove_shortcode_options' );
 } 
 
 ?>

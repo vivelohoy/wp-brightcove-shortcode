72770b14bdf70e257961574e74cda45c470beaa5
Nick Bennett <nbennett@vivelohoy.com>
2015-03-03 11:21:32 -0600

    Remove options and instead use plugin globals.

--- wp-brightcove-shortcode.php
+++ wp-brightcove-shortcode.php
@@ -20,60 +20,27 @@ if( !defined( 'wp_brightcove_shortcode_URL' ) ) {
     define('wp_brightcove_shortcode_URL', plugin_dir_url( __FILE__ ) ); // plugin url
 }
 
+// source: https://videocloud.brightcove.com/publishing
+$wbs_brightcove_players = array(
+    "vivelohoy"         => array(
+        "player_id"     => 4005130149001,
+        "player_key"    => 'AQ~~,AAAB2Ejp1kE~,qYgZ7QVyRmCtORwOH7VtCKYNUwwP3qno',
+    ),
+    "vivelohoy-noads"   => array(
+        "player_id"     => 2027711527001,
+        "player_key"    => 'AQ~~,AAAB2Ejp1kE~,qYgZ7QVyRmCflxEtsSSb7N6jXd3aEUNg',
+    ),
+    "emprendedores"     => array(
+        "player_id"     => 3971228038001,
+        "player_key"    => 'AQ~~,AAAB2Ejp1kE~,qYgZ7QVyRmAY6eVE_jKAzK_NU0a57Pd6',
+    ),
+);
 
-/*
-
-IIIII NN   NN IIIII TTTTTTT
- III  NNN  NN  III    TTT
- III  NN N NN  III    TTT
- III  NN  NNN  III    TTT
-IIIII NN   NN IIIII   TTT
-
-*/
-
-// Set-up Action and Filter Hooks
-register_uninstall_hook( __FILE__, 'wp_brightcove_shortcode_delete_plugin_options' );
-// On deactivation, delete all settings. This is't suitable for production.
-register_deactivation_hook( __FILE__, 'wp_brightcove_shortcode_delete_plugin_options' );
-register_activation_hook( __FILE__, 'wp_brightcove_shortcode_add_defaults' );
-
-
-// Delete options table entries ONLY when plugin deactivated AND deleted
-function wp_brightcove_shortcode_delete_plugin_options() {
-    delete_option( 'wp_brightcove_shortcode_options' );
-}
-
-// Define default option settings
-function wp_brightcove_shortcode_add_defaults() {
-    $tmp = get_option( 'wp_brightcove_shortcode_options' );
-    if( !is_array( $tmp ) ) {
-        delete_option( 'wp_brightcove_shortcode_options' );
-        /*
-        Brightcove Players:
-
-        Vivelohoy Video Player with Ads - Chromeless, 480x270 (default)
-        player_id = 4005130149001
-        player_key = AQ~~,AAAB2Ejp1kE~,qYgZ7QVyRmCtORwOH7VtCKYNUwwP3qno
-
-        Vivelohoy Video Player - Chromeless, 480x270
-        player_id = 2027711527001
-        player_key = AQ~~,AAAB2Ejp1kE~,qYgZ7QVyRmCflxEtsSSb7N6jXd3aEUNg
-
-        Emprendedores Video Player - Chromeless, 480x270
-        player_id = 3971228038001
-        player_key = AQ~~,AAAB2Ejp1kE~,qYgZ7QVyRmAY6eVE_jKAzK_NU0a57Pd6
-
-        source: https://videocloud.brightcove.com/publishing
-        */
-        $arr = array(
-            'video_width'   => 853,
-            'video_height'  => 480,
-            'player_id'     => 4005130149001,
-            'player_key'    => 'AQ~~,AAAB2Ejp1kE~,qYgZ7QVyRmCtORwOH7VtCKYNUwwP3qno'
-        );
-        update_option( 'wp_brightcove_shortcode_options', $arr );
-    }
-}
+$wbs_video_defaults = array(
+    'id'        => false,
+    'width'     => 860,
+    'height'    => 484,
+);
 
 
 /*
@@ -89,27 +56,19 @@ SS      HH   HH OO   OO RR   RR   TTT   CC    C OO   OO DD  DD  EE
 
 if( !function_exists( 'brightcove_video_shortcode' ) ) {
     function brightcove_video_shortcode( $atts ) {
+        global $wbs_brightcove_players;
+        global $wbs_video_defaults;
+
+        $atts = shortcode_atts( $wbs_video_defaults, $atts );
+
         if( array_key_exists( 'id', $atts ) && $atts['id'] ) {
-            $options = get_option( 'wp_brightcove_shortcode_options' );
-
-            $atts = shortcode_atts(
-                    array(
-                        'id'            => false,
-                        'video_width'   => $options['video_width'],
-                        'video_height'  => $options['video_height'],
-                        'player_id'     => $options['player_id'],
-                        'player_key'    => $options['player_key']
-                    ),
-                    $atts
-                );
-
             $context = Timber::get_context();
             $timber_options = array(
-                'VIDEO_WIDTH'       => $atts['video_width'],
-                'VIDEO_HEIGHT'      => $atts['video_height'],
+                'VIDEO_WIDTH'       => $atts['width'],
+                'VIDEO_HEIGHT'      => $atts['height'],
                 'VIDEO_ID'          => $atts['id'],
-                'PLAYER_ID'         => $atts['player_id'],
-                'PLAYER_KEY'        => $atts['player_key']
+                'PLAYER_ID'         => $wbs_brightcove_players['vivelohoy']['player_id'],
+                'PLAYER_KEY'        => $wbs_brightcove_players['vivelohoy']['player_key'],
             );
             $context = array_merge( $context, $timber_options );
 
